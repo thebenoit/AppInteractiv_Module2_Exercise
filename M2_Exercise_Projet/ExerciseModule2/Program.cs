3b72f8ef3e1aced9ec1a413916cc8d3ae1f7de49
tyl3n <123915347+amcnabbbaltar@users.noreply.github.com>
2024-08-26 10:22:54 -0400

    Exercise 2.

---
+++ M2_Exercise_Projet/ExerciseModule2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+class Program()
+{
+    const uint NOMBRE_ITERATIONS = 100000; // Le nombre d'itérations
+    public static void Main(string[] args)
+    {
+        Stopwatch sw = new Stopwatch(); // Pour évaluer le temps des opérations
+                                        // On va concaténer les nombres de 0 à NOMBRE ITERATIONS-1 dans une
+                                        // chaîne de caractères
+        sw.Start();
+        string premiereChaine = "";
+        for (int i = 0; i < NOMBRE_ITERATIONS; i++)
+        {
+            premiereChaine = premiereChaine + i + ' ';
+        }
+        sw.Stop();
+        Console.WriteLine($"La concatenation a pris {sw.ElapsedMilliseconds} millisecondes");
+        // On fait la même chose mais cette fois avec un StringBuilder
+        sw.Restart();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < NOMBRE_ITERATIONS; i++)
+        {
+            sb.Append(i);
+            sb.Append(' ');
+        }
+        string deuxiemeChaine = sb.ToString();
+        sw.Stop();
+        Console.WriteLine($"L'utilisation de StringBuilder a pris {sw.ElapsedMilliseconds} millisecondes");
+    }
+}
